bbb0c5497705a5dd7af66a8eb74414a96bed5f23
kazordomo <zak.eklund@live.se>
2022-08-17 19:48:10 +0200

    feat: startup script

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,44 @@ extern crate penrose;
 
 use penrose::{
     core::{
-        bindings::MouseEvent, config::Config, helpers::index_selectors, manager::WindowManager,
+        bindings::MouseEvent, config::Config, helpers::index_selectors, hooks::Hooks, hooks::Hook,
+        manager::WindowManager, xconnection::XConn,
     },
     logging_error_handler,
+    spawn,
     xcb::new_xcb_backed_window_manager,
-    Backward, Forward, Less, More, Result,
+    Backward, Forward, Less, More, Result, XcbConnection,
 };
 
+use simplelog::{LevelFilter, SimpleLogger};
+
+pub struct StartupScript {
+    path: String,
+}
+
+impl StartupScript {
+    pub fn new(s: impl Into<String>) -> Self {
+        Self { path: s.into() }
+    } 
+}
+
+impl <X: XConn> Hook<X> for StartupScript {
+    fn startup(&mut self, _: &mut WindowManager<X>) -> Result<()> {
+        spawn!(&self.path)
+    }
+}
+
+const TERMINAL: &str = "alacritty";
+const LAUNCHER: &str = "dmenu_run";
+const STARTUP_HOOK_PATH: &str = "$HOME/zwm";
+
 fn main() -> Result<()> {
+    if let Err(e) = SimpleLogger::init(LevelFilter::Info, simplelog::Config::default()) {
+        panic!("unable to set log level: {}", e)
+    };
+
     let config = Config::default();
-    let hooks = vec![];
+    let hooks: Hooks<XcbConnection> = vec![Box::new(StartupScript::new(STARTUP_HOOK_PATH))];
 
     let key_bindings = gen_keybindings! {
         "M-j" => run_internal!(cycle_client, Forward);
@@ -32,8 +60,8 @@ fn main() -> Result<()> {
         "M-A-Right" => run_internal!(update_main_ratio, More);
         "M-A-Left" => run_internal!(update_main_ratio, Less);
         "M-A-Escape" => run_internal!(exit);
-        "M-semicolon" => run_external!("dmenu_run");
-        "M-Return" => run_external!("st");
+        "M-Return" => run_external!(TERMINAL);
+        "M-p" => run_external!(LAUNCHER);
 
         map: { "1", "2", "3", "4", "5", "6", "7", "8", "9" } to index_selectors(9) => {
             "M-{}" => focus_workspace (REF);
